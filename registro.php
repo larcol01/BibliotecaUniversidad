ef704782dfbc79fccb3c7f762a3cf23496ff6df4
daw202 <larcol01@educantabria.es>
2024-04-02 20:01:37 +0200

    conexion hecha a la base de datos en la clase de registro y  intento de insert parte 2

--- registro.php
+++ registro.php
@@ -47,16 +47,7 @@ and open the template in the editor.
             <br>
             <br>
             
-            
-            <input type="radio" name="alumno" value="1">Alumno
-            <br>
-            <input type="radio" name="Alumno_doctorado/Proyecto_fin_de_carrera" value="2">Alumno doctorado / Proyecto fin de carrera
-            <br>
-            <input type="radio" name="profesor" value="3">Profesor
-            <br>
-            <input type="radio" name="autor" value="4">Autor
-            <br>
-           <br>
+          
             
             <input type="submit" value="Registrar" name="enviar" /><br><br>
             <hr>
@@ -71,32 +62,15 @@ and open the template in the editor.
             /*Aqui se obtiene la conexion de la base de datos utilizando la funcion getConexion() que he creado en otro php diferente*/
             $conexion  = getConnexion();
             /*Aqui compruebo si se ha enviado los datos del formulario del nuevo usuario, si es asi entra dentro*/
-            if (isset($_POST['enviar']))
-            {
-                if ($_REQUEST['alumno'] == "1") {
-                    $tipoAlumno = 1;
-                    echo  $tipoAlumno;
-                 } 
-                 if($_REQUEST['Alumno_doctorado/Proyecto_fin_de_carrera'] == "2"){
-                     $tipoAlumno = 2;
-                    echo  $tipoAlumno;
-                 }
-                 if($_REQUEST['profesor'] == "3"){
-                     $suma = 3;
-                    echo  $tipoAlumno;
-                 }
-                 if($_REQUEST['autor'] == "4"){
-                     $suma = 4;
-                    echo  $tipoAlumno;
-                 }
+            
                  
                 /*compruebo que los campos no esten vacios y si es asi hago un insert para introducir los datos correspondientes*/
                 if(isset($_REQUEST['nombre']) && isset($_REQUEST['apellido']) && isset($_REQUEST['dni']) && isset($_REQUEST['contraena']) 
                     && isset($_REQUEST['email']) && isset($_REQUEST['telefono']) && isset($_REQUEST['alumno']) || isset($_REQUEST['Alumno_doctorado/Proyecto_fin_de_carrera']) 
                         || isset($_REQUEST['profesor']) || isset($_REQUEST['autor']) )
                 {
-                    $consulta = "insert into usuario(nombre, apellido, dni, contrasena, email, telefono, dni) "
-                        . "VALUES ('" . $_REQUEST['nombre'] . "','" . $_REQUEST['apellido'] . "', '" . $_REQUEST['usuario'] 
+                    $consulta = "insert into usuario(nombre, apellido,contraseña,email,telefono,dni) "
+                        . "VALUES ('" . $_REQUEST['nombre'] . "','" . $_REQUEST['apellido']   
                         . "','" . $_REQUEST['contrasena'] . "','" . $_REQUEST['email'] . "','" . $_REQUEST['telefono'] . "','" . $_REQUEST['dni'] . "')";
 
 
@@ -109,7 +83,7 @@ and open the template in the editor.
 
 
 
-            }
+            
         
         
         
